cd1277e89df785b3cf3f5de5a0fa5404dd4339bc
Boivin <boivinhelene@yahoo.com>
2018-10-12 14:12:40 +0200

    JS Partie 2

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,5 @@
+function efface(){
+var nom = document.getElementById("lastname").value="";
+var prenom = document.getElementById("firstname").value="";
+var ville = document.getElementById("city").value="";
+}
